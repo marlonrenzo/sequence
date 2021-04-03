1bd47afa2fd3c8b1230f0c1add2091480a415885
marlonrenzo <mfajardo3@my.bcit.ca>
2021-04-03 00:04:21 -0700

    Fixed some typos in the routing definitions

--- app.js
+++ app.js
@@ -39,15 +39,15 @@ app.put('/scores', function(req, res) {
     res.end("Upload a new entry into the scores table");
 });
 
-app.put('/users', function(req, res) {
+app.put('/users/:username', function(req, res) {
     res.end("Adds a new entry into the user table");
 });
 
-app.post('users/:username', function(req, res) {
+app.post('/users/:username', function(req, res) {
     res.end("Check existence in database, then login");
 });
 
-app.post('users/:username/:newname', function(req, res) {
+app.post('/users/change/:username/:newname', function(req, res) {
     res.end("Change the name of a stored user.");
 })
 
@@ -55,7 +55,7 @@ app.post('/users/:username/:pw', function(req, res) {
     res.end("Authenticates credentials, and displays stats for all endpoints")
 });
 
-app.delete('/users/:userid', function(req, res) {
+app.delete('/users/:username', function(req, res) {
     res.end("Delete a user and all its scores from the tables");
 });
 
