5497a75bd8853b320017abfbeb523a975dc68f50
marlonrenzo <mfajardo3@my.bcit.ca>
2021-04-03 01:40:45 -0700

    Removed unneeded params

--- sql/schema.sql
+++ sql/schema.sql
@@ -52,11 +52,11 @@ Accessed by PUT request to /scores
 */
 DROP PROCEDURE IF EXISTS add_score;
 DELIMITER //
-CREATE PROCEDURE add_score(IN current_username VARCHAR(30), IN new_score INTEGER, OUT result INTEGER)
+CREATE PROCEDURE add_score(IN current_username VARCHAR(30), IN new_score INTEGER)
 BEGIN
     INSERT INTO scores(userID, score_time)
     VALUES ((SELECT id FROM users WHERE username = current_username), new_score);
-    SELECT ROW_COUNT() INTO result;
+    SELECT ROW_COUNT();
 END//
 DELIMITER ;
 
@@ -67,10 +67,10 @@ Accessed by PUT request to /users
 */
 DROP PROCEDURE IF EXISTS add_user;
 DELIMITER //
-CREATE PROCEDURE add_user(IN new_username VARCHAR(30), OUT result INTEGER);
+CREATE PROCEDURE add_user(IN new_username VARCHAR(30));
 BEGIN
     INSERT INTO users (username) VALUES (new_username);
-    SELECT ROW_COUNT() INTO result;
+    SELECT ROW_COUNT();
 END//
 DELIMITER ;
 
@@ -99,12 +99,12 @@ Accessed by POST request to users/change/
 */
 DROP PROCEDURE IF EXISTS change_name;
 DELIMITER //
-CREATE PROCEDURE change_name(IN old_username VARCHAR(30), IN new_username VARCHAR(30), OUT result INTEGER);
+CREATE PROCEDURE change_name(IN old_username VARCHAR(30), IN new_username VARCHAR(30));
 BEGIN
     UPDATE users
     SET username = new_username
     WHERE username = old_username;
-    SELECT ROW_COUNT() INTO result;
+    SELECT ROW_COUNT();
 END//
 DELIMITER ;
 
@@ -133,10 +133,10 @@ Accessed by DELETE request to /users
 */
 DROP PROCEDURE IF EXISTS delete_user;
 DELIMITER //
-CREATE PROCEDURE delete_user(IN input_name VARCHAR(30), OUT result INTEGER);
+CREATE PROCEDURE delete_user(IN input_name VARCHAR(30));
 BEGIN
     DELETE FROM users WHERE username = input_name;
-    SELECT ROW_COUNT() INTO result;
+    SELECT ROW_COUNT();
 END//
 DELIMITER ;
 
@@ -148,9 +148,9 @@ Accessed by DELETE request to /scores
 */
 DROP PROCEDURE IF EXISTS delete_score;
 DELIMITER //
-CREATE PROCEDURE delete_score(IN score_id VARCHAR(30), OUT result INTEGER);
+CREATE PROCEDURE delete_score(IN score_id VARCHAR(30));
 BEGIN
     DELETE FROM scores WHERE id = score_id;
-    SELECT ROW_COUNT() INTO result;
+    SELECT ROW_COUNT();
 END//
 DELIMITER ;
