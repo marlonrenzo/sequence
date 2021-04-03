a793c0c01fee72cdeafe96cfd62bec75d50f87cc
marlonrenzo <mfajardo3@my.bcit.ca>
2021-04-03 00:03:58 -0700

    Added the rest of the procedures an queries

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(30) NOT NULL,
-
+    pswrd VARCHAR(30)
 );
 
 CREATE TABLE scores (
@@ -12,7 +12,7 @@ CREATE TABLE scores (
 );
 
 
--- STORED PROCEDURES and FUNCTIONS-- 
+-- PROCEDURES, FUNCTIONS and Queries-- 
 
 
 /*
@@ -34,6 +34,119 @@ DELIMITER //
 CREATE PROCEDURE get_user_scores(IN current_username VARCHAR(30))
 BEGIN
     SELECT * FROM scores
-    WHERE userID = (SELECT id FROM users WHERE username = current_username)
+    WHERE userID = (SELECT id FROM users WHERE username = current_username);
+END//
+DELIMITER ;
+
+
+/*
+Gets a list of users.
+Accessed by GET request to /users
+*/
+SELECT username FROM users; 
+
+
+/*
+Inserts a new score into the database
+Accessed by PUT request to /scores
+*/
+DROP PROCEDURE IF EXISTS add_score;
+DELIMITER //
+CREATE PROCEDURE add_score(IN current_username VARCHAR(30), IN new_score INTEGER)
+BEGIN
+    INSERT INTO scores(userID, score_time)
+    VALUES ((SELECT id FROM users WHERE username = current_username), new_score);
+END//
+DELIMITER ;
+
+
+/*
+Inserts a new user into the database
+Accessed by PUT request to /users
+*/
+DROP PROCEDURE IF EXISTS add_user;
+DELIMITER //
+CREATE PROCEDURE add_user(IN new_username VARCHAR(30));
+BEGIN
+    INSERT INTO users (username)
+    VALUES (new_username)
+END//
+DELIMITER ;
+
+
+/*
+Check existence in database then login
+Accessed by POST request to /users
+*/
+DROP FUNCTION IF EXISTS user_exists;
+DELIMITER //
+CREATE FUNCTION user_exists(input_name VARCHAR(30)) 
+RETURNS INT READS SQL DATA
+BEGIN
+    RETURN (
+      SELECT 1
+      FROM users
+      WHERE username = input_name
+    );
+END //
+DELIMITER ;
+
+
+/*
+Change the name of a user
+Accessed by POST request to users/change/
+*/
+DROP PROCEDURE IF EXISTS change_name;
+DELIMITER //
+CREATE PROCEDURE change_name(IN old_username VARCHAR(30), IN new_username VARCHAR(30));
+BEGIN
+    UPDATE users
+    SET username = new_username
+    WHERE username = old_username;
+END//
+DELIMITER ;
+
+
+/*
+Authenticate credentials
+Accessed by POST request to /users
+*/
+DROP FUNCTION IF EXISTS authenticate;
+DELIMITER //
+CREATE FUNCTION authenticate(input_name VARCHAR(30), passwrd VARCHAR(30)) 
+RETURNS INT READS SQL DATA
+BEGIN
+    RETURN (
+      SELECT 1
+      FROM users
+      WHERE username = input_name AND pswrd = passwrd
+    );
+END //
+DELIMITER ;
+
+
+/*
+Deletes a user from the database
+Accessed by DELETE request to /users
+*/
+DROP PROCEDURE IF EXISTS delete_user;
+DELIMITER //
+CREATE PROCEDURE delete_user(IN input_name VARCHAR(30));
+BEGIN
+    DELETE FROM users WHERE username = input_name;
+END//
+DELIMITER ;
+
+
+
+/*
+Deletes a score from the database
+Accessed by DELETE request to /scores
+*/
+DROP PROCEDURE IF EXISTS delete_score;
+DELIMITER //
+CREATE PROCEDURE delete_score(IN score_id VARCHAR(30));
+BEGIN
+    DELETE FROM scores WHERE id = score_id;
 END//
 DELIMITER ;
