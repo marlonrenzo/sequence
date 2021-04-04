ab0af84bbcefcf28da361bbb732cfb14961ffdac
marlonrenzo <mfajardo3@my.bcit.ca>
2021-04-03 22:32:18 -0700

    Modified endpoints and renamed file

--- serverMain.js
+++ serverMain.js
@@ -1,11 +1,40 @@
+const ENDPOINT = "/sequence/v1";
 const USER_ENDPOINT = "/sequence/users";
-const app = require('../modules/appSetup');
-const db = require('../modules/db');
+
+const app = require('./modules/appSetup');
+const db = require('./modules/db');
 
 // --- Routing ---
 
+// Get leaderboard of top 10 scores
+app.get(ENDPOINT + '/scores', function(req, res) {
+    db.connect(function (err) {
+        if (err) throw err;
+        let sql = "SELECT * FROM get_scores";
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            let resultText = JSON.stringify(result);
+            res.end(resultText);
+        });
+    });
+});
+
+// Get the scores for a user
+app.get(ENDPOINT + '/scores/:username', function(req, res) {
+    let name = req.params.username;
+    db.connect(function (err) {
+        if (err) throw err;
+        let sql = `CALL get_user_scores('${name}')`;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            let resultText = JSON.stringify(result);
+            res.end(resultText);
+        });
+    });
+});
+
 // Get the list of users
-app.get(USER_ENDPOINT, function(req, res) {
+app.get(ENDPOINT + '/users', function(req, res) {
     db.connect(function (err) {
         if (err) throw err;
         let sql = "SELECT username FROM users;";
@@ -17,8 +46,23 @@ app.get(USER_ENDPOINT, function(req, res) {
     });
 });
 
+// Upload a new entry into the scores table
+app.put(ENDPOINT + '/scores/:username/:score', function(req, res) {
+    let name = req.params.username;
+    let value = req.params.score;
+    db.connect(function (err) {
+        if (err) throw err;
+        let sql = `CALL add_score('${name}', ${value});`;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            let resultText = JSON.stringify(result);
+            res.end(resultText);
+        });
+    });
+});
+
 // Adds a new entry into the user table
-app.put(USER_ENDPOINT + '/:username', function(req, res) {
+app.put(ENDPOINT + '/users/:username', function(req, res) {
     let name = req.params.username;
     db.connect(function (err) {
         if (err) throw err;
@@ -32,7 +76,7 @@ app.put(USER_ENDPOINT + '/:username', function(req, res) {
 });
 
 // Check existence in database, then login
-app.post(USER_ENDPOINT + '/:username', function(req, res) {
+app.post(ENDPOINT + '/users/:username', function(req, res) {
     let name = req.params.username;
     db.connect(function (err) {
         if (err) throw err;
@@ -46,7 +90,7 @@ app.post(USER_ENDPOINT + '/:username', function(req, res) {
 });
 
 // Change the name of a stored user.
-app.post(USER_ENDPOINT + '/change/:username/:newname', function(req, res) {
+app.post(ENDPOINT + '/users/change/:username/:newname', function(req, res) {
     let name = req.params.username;
     let newname = req.params.newname;
     db.connect(function (err) {
@@ -61,7 +105,7 @@ app.post(USER_ENDPOINT + '/change/:username/:newname', function(req, res) {
 })
 
 // Authenticates credentials, and displays stats for all endpoints
-app.post(USER_ENDPOINT + '/authenticate/:username/:pw', function(req, res) {
+app.post(ENDPOINT + '/users/authenticate/:username/:pw', function(req, res) {
     let name = req.params.username;
     let password = req.params.pw;
     db.connect(function (err) {
@@ -76,7 +120,7 @@ app.post(USER_ENDPOINT + '/authenticate/:username/:pw', function(req, res) {
 });
 
 // Delete a user and all its scores from the tables
-app.delete(USER_ENDPOINT + '/:username', function(req, res) {
+app.delete(ENDPOINT + '/users/:username', function(req, res) {
     let name = req.params.username;
     db.connect(function (err) {
         if (err) throw err;
@@ -89,4 +133,19 @@ app.delete(USER_ENDPOINT + '/:username', function(req, res) {
     });
 });
 
+// Delete a score from the database
+app.delete(ENDPOINT + '/scores/:scoreID', function(req, res) {
+    let scoreID = req.params.scoreID;
+    db.connect(function (err) {
+        if (err) throw err;
+        let sql = `CALL delete_score(${scoreID});`;
+        db.query(sql, function (err, result) {
+            if (err) throw err;
+            let resultText = JSON.stringify(result);
+            res.end(resultText);
+        });
+    });
+});
+
 app.listen();
+ 
