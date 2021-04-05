d95fdbba0379a7e8887bd8aac1305442bfcdc8a4
marlonrenzo <mfajardo3@my.bcit.ca>
2021-04-04 18:43:45 -0700

    Added game logic and button functionality

--- public/script/gamePage.js
+++ public/script/gamePage.js
@@ -1,18 +1,42 @@
-function createNewButton(number) {
+function createNewButton(number, xPos, yPos) {
     let div = document.getElementById("gameWindow");
     let btn = document.createElement("button");
     btn.id = "btn" + number;
     btn.className = "sequenceButton"
     btn.onclick = function () { removeButtonAfterClick(btn.id) };
     btn.innerHTML = `<div id="div${number}">${number}</div>`;
-    // btn.style.position = 'relative';
-    // btn.style.marginTop = '400px';
-    // btn.style.marginLeft = '400px';
+    btn.style.position = 'absolute';
+    btn.style.marginTop = `${yPos}px`;
+    btn.style.marginLeft = `${xPos}px`;
     div.appendChild(btn);
 }
 
 function removeButtonAfterClick(id) {
-    console.log("removing" + id);
+    let clickedNumber = id.match(/\d+/)[0];
+    let currentNumber = document.getElementById("currentNumber").innerText;
+    console.log(currentNumber);
+    if (clickedNumber == currentNumber) {
+        currentNumber++;
+        document.getElementById("currentNumber").innerText = currentNumber;
+        removeButton(id);
+    } else {
+        wrongButtonClicked(id);
+    }
+}
+
+function wrongButtonClicked(id) {
+    let currentElm = document.getElementById(id);
+    currentElm.style.animation = "shake 0.5s";
+    document.body.style.backgroundColor = "#734343";
+
+    setTimeout(function() {
+        document.body.style.backgroundColor = "#333333";
+        document.body.style.transition = "200ms";
+        currentElm.style.animation = "none";
+    }, 750);
+}
+
+function removeButton(id) {
     let div = document.getElementById("gameWindow");
     let currentElm = document.getElementById(id);
     document.getElementById(id).style = `background-color: #333333;
@@ -24,10 +48,20 @@ function removeButtonAfterClick(id) {
 }
 
 function spawnButtons() {
-    btn.style.position = 'absolute';
-    for(let i=10; i>=1; i--) {
-        console.log(i);
+    let div = document.getElementById("gameWindow");
+    let yLimit = div.offsetHeight - 100;
+    let xLimit = div.offsetWidth - 100;
+    console.log("Y limit:" + yLimit + "\nX limit: " + xLimit);
+    for(let num=50; num>=1; num--) {
+        let randomX = randomNumber(xLimit);
+        let randomY = randomNumber(yLimit);
+        createNewButton(num, randomX, randomY);
     }
 }
 
-createNewButton(1);
+function randomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// createNewButton(1);
+spawnButtons();
