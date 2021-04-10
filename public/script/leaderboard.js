0f10025ba7c7f1203c53a65295e5fbe317eed0e6
marlonrenzo <marlonrfajardo@gmail.com>
2021-04-09 19:26:47 -0700

    added high score for the current user

--- public/script/leaderboard.js
+++ public/script/leaderboard.js
@@ -1,4 +1,26 @@
-var url = "https://diyarsalamatravandi.ca/sequence/v1";
+const url = "https://diyarsalamatravandi.ca/sequence/v1";
+let username;
+
+function checkIfLoggedIn() {
+    username = localStorage.getItem("username");
+    if(username == null) {
+        window.location.replace("https://marlonfajardo.ca/sequence");
+    }
+}
+
+function getHighScore(scores) {
+    checkIfLoggedIn();
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url + `/scores/${username}`, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let highScore = JSON.parse(this.responseText)[0][0];
+            console.log(highScore);
+            fillLeaderboard(scores, highScore);
+        }
+    }
+}
 
 function getScores() {
     const xhttp = new XMLHttpRequest();
@@ -6,17 +28,17 @@ function getScores() {
     xhttp.send();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(JSON.parse(this.responseText))
-            fillLeaderboard(JSON.parse(this.responseText));
+            getHighScore(JSON.parse(this.responseText));
         }
     }
 }
 
-function createSpanElement(type, number, text) {
+function createSpanElement(type, number, text, color) {
     let element = document.createElement("span");
     element.className = type;
     element.id = `${type}${number}`;
     element.innerText = text;
+    element.style.color = color;
     return element;
 }
 
@@ -31,10 +53,11 @@ function createCard(place, name, time, number) {
 }
 
 function createLeaderboardEntry(scoreObj, place) {
+    const color = "white";
     let window = document.getElementById("leaderboard");
-    let placeText = createSpanElement("place", place, place);
-    let usernameText = createSpanElement("username", place, scoreObj['user']);
-    let timeText = createSpanElement("time", place, scoreObj['score']);
+    let placeText = createSpanElement("place", place, place, color);
+    let usernameText = createSpanElement("username", place, scoreObj['user'], color);
+    let timeText = createSpanElement("time", place, scoreObj['score'], color);
     let card = createCard(placeText, usernameText, timeText, place);
     window.appendChild(card);
 }
@@ -47,17 +70,35 @@ function getSize(scores) {
     }
 }
 
-function fillLeaderboard(scores) {
+function displayHighScore(highScore) {
+    let color = "#EAAA00";
+    let place = highScore['place'];
+    let window = document.getElementById("leaderboard");
+    let placeText = createSpanElement("place", place, place, color);
+    let usernameText = createSpanElement("username", place, highScore['user'], color);
+    let timeText = createSpanElement("time", place, highScore['score'], color);
+    let card = createCard(placeText, usernameText, timeText, place);
+    window.appendChild(card);
+}
+
+function fillLeaderboard(scores, highScore) {
+    let userIsInTop10 = false;
     let place = 1;
     let leaderBoardSize = getSize(scores);
     document.getElementById("leaderboard").innerHTML = "";
     for (let index=0; index<leaderBoardSize; index++) {
+        if (scores[index]['user'] == username) {
+            userIsInTop10 = true;
+        }
         createLeaderboardEntry(scores[index], place);
         place++;
+    }
+    if (!userIsInTop10) {
+        displayHighScore(highScore);
     }
 }
 
 getScores();
 document.getElementById("back").onclick = function () {
-    window.location.replace("https://marlonfajardo.ca/sequence");
+    window.location.replace("https://marlonfajardo.ca/sequence/game");
 }
