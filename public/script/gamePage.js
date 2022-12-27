cb1133b2ddeeee69956edd3db5300d0892a66848
marlonrenzo <mfajardo3@my.bcit.ca>
2022-12-26 22:42:34 -0800

    all content fades out

--- public/script/gamePage.js
+++ public/script/gamePage.js
@@ -249,6 +249,9 @@ function uploadScore() {
 }
 
 function visitLeaderboard() {
+    setTimeout(function() {
+        document.getElementById("contentDiv").style.animation = "fade-out 1s;";
+    }, 1000);
     window.location.replace("https://marlonfajardo.ca/sequence/leaderboard");
 }
 
