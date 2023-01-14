ffda1c7a404fb233b3b5aaf2dafb0fc508dec35c
marlonrenzo <mfajardo3@my.bcit.ca>
2023-01-13 22:09:40 -0800

    fixed logic error

--- public/script/leaderboard.js
+++ public/script/leaderboard.js
@@ -363,7 +363,7 @@ function getHighScore(scores) {
   xhttp.send();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      let highScores = JSON.parse(this.responseText)[0][0];
+      let highScores = JSON.parse(this.responseText)[0];
       console.log(highScores);
       fillLeaderboards(scores, highScores);
     }
